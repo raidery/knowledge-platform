3d48a4c7284dff50ed983cbad94910db5a3d40ac
Raidery <raidery@gmail.com>
2026-04-30 10:51:00 +0800

    Revert "feat(web): add KbService as top-level menu with ingest route"
    This reverts commit 4cb76f585b1ab79b65782847432b3fe756ec7292.

--- web/src/views/kb_service/route.js
+++ web/src/views/kb_service/route.js
@@ -1,22 +1,11 @@
-export default [
-  {
-    path: '/kb_service',
-    name: 'KbService',
-    redirect: '/kb_service/ingest',
-    meta: {
-      title: '知识库服务',
-      icon: 'carbon:document',
-      order: 2,
+export default {
+  path: '/kb_service',
+  name: 'KbService',
+  children: [
+    {
+      path: 'ingest',
+      name: 'KbIngest',
+      component: () => import('@/views/kb_service/ingest.vue'),
     },
-    children: [
-      {
-        path: 'ingest',
-        name: 'KbIngest',
-        component: () => import('@/views/kb_service/ingest.vue'),
-        meta: {
-          title: '文档上传',
-        },
-      },
-    ],
-  },
-]
+  ],
+}
